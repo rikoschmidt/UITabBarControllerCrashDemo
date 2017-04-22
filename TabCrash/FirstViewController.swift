02b5e426f1ae22fde53c8ca582b2ace363d025d0
Riko Schmidt <riko.schmidt@lidl.com>
2017-04-22 23:32:56 +0200

    Initial Commit

---
+++ TabCrash/FirstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  TabCrash
+//
+//  Created by Riko Schmidt on 22/04/17.
+//  Copyright © 2017 Riko Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
